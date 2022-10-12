bc8f14921a98571455c795a92bde2d840518f641
Suvigya Saxena <suvigya2001@gmail.com>
2022-10-12 21:30:04 +0530

    mongo url fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
 
-mongoose.connect(DATABASE_URL);
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
 
 const Message = mongoose.model('Message', { message: String });
 
